3edc44386951c3fac238cdf4e3565ead693304ce
Aloys Berger <aloys.berger@technichegroup.com>
2023-01-04 11:35:43 +1000

    feat: Add tailwind, webcomponents (lit) and about page
    This is part of testing the CI/CD pipeline and will be merged into the main branch as part of that test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
-
+import lit from "@astrojs/lit";
+// https://astro.build/config
+import mdx from "@astrojs/mdx";
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-import mdx from "@astrojs/mdx";
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()]
+   integrations: [lit(), tailwind(), mdx()],
 });
