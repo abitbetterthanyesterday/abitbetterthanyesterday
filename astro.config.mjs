b9df987faeef18f9e8b825a7a4aafb27a00fa0bf
Aloys Berger <alo@reaktive.com>
2023-01-12 22:52:04 +1000

    style: Set up shiki for code hiligting
    Improve styling throughout [wip!]

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,10 +6,16 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+   markdown:{
+      shikiConfig:{
+         theme:'one-dark-pro',
+         wrap:true,
+      }
+   },
    integrations: [
       tailwind({
          config: { applyBaseStyles: false },
       }),
-      mdx(),
+      mdx()
    ],
 });
