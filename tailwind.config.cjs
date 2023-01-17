8c7adc4125f2c167a9381591fd56f9150a3acf60
Aloys Berger <aloys.berger@technichegroup.com>
2023-01-17 12:47:42 +1000

    style(tailwind): Improve tailwind's custom brand colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,24 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-   darkMode: "class",
-   theme: {
-      extend: {
-         colors: {
-            brand: {
-               backgroundDark: "#10181cff",
-               background: "#17242cff",
-               backgroundLight: "#283c49",
-               subtle: "#526074ff",
-               title: "#ecededff",
-               body: "#fcfcfcff",
-               secondaryDark: "#2a7275ff",
-               secondary: "#2b7d7dff",
-               secondaryLight: "#bfe8e9ff",
-               accent: "#e0c463ff",
-            },
-         },
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          primary: {
+            dark: "#10181cff",
+            DEFAULT: "#17242cff",
+            light: "#283c49",
+          },
+          white: {
+            subtle: "#526074ff",
+            title: "#ecededff",
+            body: "#fcfcfcff",
+          },
+          secondary: {
+            dark: "#2a7275ff",
+            DEFAULT: "#2b7d7dff",
+            light: "#bfe8e9ff",
+          },
+          accent: {
+            DEFAULT: "#e0c463ff",
+          },
+        },
       },
-      plugins: [],
-   },
+    },
+    plugins: [],
+  },
 };
