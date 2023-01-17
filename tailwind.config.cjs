d5bbe4826d34a1d6e61db481dcd7b797da0b3f9d
Aloys Berger <aloys.berger@technichegroup.com>
2023-01-17 12:32:56 +1000

    style(tailwind): Add brand colors to tailwind's config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,22 @@ module.exports = {
    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
    darkMode: "class",
    theme: {
-      extend: {},
+      extend: {
+         colors: {
+            brand: {
+               backgroundDark: "#10181cff",
+               background: "#17242cff",
+               backgroundLight: "#283c49",
+               subtle: "#526074ff",
+               title: "#ecededff",
+               body: "#fcfcfcff",
+               secondaryDark: "#2a7275ff",
+               secondary: "#2b7d7dff",
+               secondaryLight: "#bfe8e9ff",
+               accent: "#e0c463ff",
+            },
+         },
+      },
+      plugins: [],
    },
-   plugins: [],
 };
