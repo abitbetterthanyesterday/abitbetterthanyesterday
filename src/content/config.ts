274e23455e8cefaf472cb149fbe48bcf211a4e81
Aloys Berger <aloys.berger@technichegroup.com>
2023-02-21 13:57:13 +1000

    feat: Improve the toggle switch
    It also includes some minor work on the schema (for integration with obsidian-to-astro)

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,14 @@
 import { defineCollection, z } from "astro:content";
 const blogCollection = defineCollection({
-  schema: z.object({
-    title: z.string(),
-    status: z.enum(["publish"]),
-    tags: z.array(z.string()),
-    created_at: z.date(),
-    last_modified_at: z.date(),
-    published_at: z.string().transform((str: string) => new Date(str))
-      .optional(),
-    lang: z.enum(["en-AU"]),
-    description: z.string().optional().nullable(),
-  }),
+   schema:z.object({
+      title: z.string(),
+      status: z.enum(["published", "draft", "private", "archived", "deleted"]),
+      tags: z.array(z.string().nullable()),
+      created_at: z.date(),
+      last_modified_at: z.date(),
+      published_at: z.date(),
+      description: z.string(),
+    })
 });
 
 export const collections = {
