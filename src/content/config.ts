8e71a51102a0573c960ffa64dd10cd154e1217b7
Aloys Berger <aloys.berger@technichegroup.com>
2023-03-02 11:14:07 +1000

    feat: Add reading time to the summary card

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,14 +1,14 @@
 import { defineCollection, z } from "astro:content";
 const blogCollection = defineCollection({
-   schema:z.object({
-      title: z.string(),
-      status: z.enum(["published", "draft", "private", "archived", "deleted"]),
-      tags: z.array(z.string().nullable()),
-      created_at: z.date(),
-      last_modified_at: z.date(),
-      published_at: z.date(),
-      description: z.string(),
-    })
+  schema: z.object({
+    title: z.string(),
+    status: z.enum(["published", "draft", "private", "archived", "deleted"]),
+    tags: z.array(z.string()),
+    created_at: z.date(),
+    last_modified_at: z.date(),
+    published_at: z.date(),
+    description: z.string(),
+  }),
 });
 
 export const collections = {
